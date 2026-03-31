8fd1250d64faf5a5cc05858d64a778f5f61a5243
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-03-31 15:12:47 +0800

    added simple structure to logs

--- pages/Log.js
+++ pages/Log.js
@@ -1,9 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { useState, useEffect } from 'react'
 
 export default function Log({ navigation }){
     return (
-        <View>
-            <Text>Log</Text>
+        <View style={styles.log_container}>
+            <View>
+                <FlatList
+                    showsVerticalScrollIndicator={true}
+                />
+            </View>
+            <View>
+                <TouchableOpacity>
+                    <Text>
+                        Search
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                        Back
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>
+                        Forward
+                    </Text>
+                </TouchableOpacity>                      
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    log_container: {
+        margin: 30,
+        backgroundColor: 'red',
+    },
+})
